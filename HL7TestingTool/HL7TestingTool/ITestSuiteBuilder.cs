bef643c4702066036967f2a4ab910156c7b957dd
Tommy Zieba <41349162+tzieba1@users.noreply.github.com>
2021-09-27 14:41:41 -0400

    Migrated to Framework 4.8 to solve compatibility issues with the nHapi.Parser.Agfa package

--- HL7TestingTool/HL7TestingTool/ITestSuiteBuilder.cs
+++ HL7TestingTool/HL7TestingTool/ITestSuiteBuilder.cs
@@ -9,7 +9,7 @@ namespace HL7TestingTool
   /// <summary>
   /// Interface supplied with methods for buiilding a test suite composed of various classes representing test cases, steps, and expected behaviours.
   /// </summary>
-  partial interface ITestSuiteBuilder
+  public interface ITestSuiteBuilder
   {
     /// <summary>
     /// Test suites need testing data including case numbers, step numbers, message, and expected results list.
@@ -23,20 +23,20 @@ namespace HL7TestingTool
     /// This method adds a test case to the test suite.
     /// </summary>
     /// <param name="caseNumber"></param>
-    public void AddTestCase(int caseNumber);
+    void AddTestCase(int caseNumber);
 
     /// <summary>
     /// Test steps are built into a specific test case based on a test case number.
     /// </summary>
     /// <param name="caseNumber"></param>
-    public void AddTestStep(int caseNumber, int stepNumber, string message);
+    void AddTestStep(int caseNumber, int stepNumber, string message);
 
     /// <summary>
     /// Expected results are built into a specific test step.
     /// This method adds an expected result to a test step.
     /// </summary>
     /// <param name="testStep"></param>
-    public void AddExpectedResults(TestStep testStep, List<ExpectedResult> expectedResults);
+    void AddExpectedResults(TestStep testStep, List<ExpectedResult> expectedResults);
 
     /// <summary>
     /// Specific test steps can have only the message updated.
@@ -44,7 +44,7 @@ namespace HL7TestingTool
     /// </summary>
     /// <param name="testStep"></param>
     /// <param name="message"></param>
-    partial void UpdateTestStep(TestStep testStep, string message);
+    //void UpdateTestStep(TestStep testStep, string message);
 
     /// <summary>
     /// Specific test steps can have only the expected results updated.
@@ -52,7 +52,7 @@ namespace HL7TestingTool
     /// </summary>
     /// <param name="testStep"></param>
     /// <param name="expectedResults"></param>
-    partial void UpdateTestStep(TestStep testStep, List<ExpectedResult> expectedResults);
+    //void UpdateTestStep(TestStep testStep, List<ExpectedResult> expectedResults);
 
     /// <summary>
     /// Specific test steps can have both the message and expected results updated.
@@ -61,7 +61,7 @@ namespace HL7TestingTool
     /// <param name="testStep"></param>
     /// <param name="message"></param>
     /// <param name="expectedResults"></param>
-    partial void UpdateTestStep(TestStep testStep, string message, List<ExpectedResult> expectedResults);
-    public List<TestStep> TestSteps { get; set; }
+    //void UpdateTestStep(TestStep testStep, string message, List<ExpectedResult> expectedResults);
+    List<TestStep> TestSteps { get; set; }
   }
 }
--- HL7TestingTool/HL7TestingTool/Program.cs
+++ HL7TestingTool/HL7TestingTool/Program.cs
@@ -20,26 +20,6 @@ namespace HL7TestingTool
   {
     static void Main(string[] args)
     {
-      /// --------------------------------------------------------------  MESSAGES
-      string TEST_CR_02_10 = @"MSH|^~\&|TEST_HARNESS^^|TEST^^|CR1|MOH_CAAT|20141104174451|TEST_HARNESS+TEST_HARNESS|ADT^A01^ADT_A01|TEST-CR-02-10|P|2.3.1
-EVN||20101020
-PID|||RJ-438^^^TEST||JOHNSTON^ROBERT^^^^^L|MURRAY^^^^^^L|19830205|M|||1220 Centennial Farm Road^^ELLIOTT^IA^51532||^PRN^PH^^^712^7670867
-PV1||I";
-
-      string TEST_CR_02_20 = @"MSH|^~\&|TEST_HARNESS|TEST|CR1|MOH_CAAT|20090223144546||QBP^Q23^QBP_Q21|TEST-CR-02-20|P|2.5
-QPD|IHE PIX Query|Q0220|RJ-438^^^&2.16.840.1.113883.3.72.5.9.1&ISO^PI
-RCP|I";
-
-      string TEST_CR_02_30 = @"MSH|^~\&|TEST_HARNESS^^|TEST^^|SANTEMPI|DOCKER|20141104174451|TEST_HARNESS+TEST_HARNESS|ADT^A01^ADT_A01|TEST-CR-02-30|P|2.3.1
-EVN||20101020
-PID|||RJ-439^^^TEST&&||JONES^JENNIFER^^^^^L|SMITH^^^^^^L|19840125|F|||123 Main Street West ^^NEWARK^NJ^30293||^PRN^PH^^^409^30495||||||
-PV1||I";
-
-      string TEST_CR_03_10 = @"MSH|^~\&|TEST_HARNESS^^|TEST^^|CR1^^|MOH_CAAT^^|20141104174451|TEST_HARNESS+TEST_HARNESS|ADT^A01^ADT_A01|TEST-CR-03-10|P|2.3.1
-EVN||20101020
-PID|||RJ-999-2^^^&2.16.840.1.113883.3.72.5.9.1&ISO||THAMES^ROBERT^^^^^L| |1983|M|||1220 Centennial Farm Road^^ELLIOTT^IA^51532||^PRN^PH^^^712^7670867
-PV1||I";
-
       /// ================================================== HOW TO USE A TERSER
       // Search - Construct a v2 message
       QBP_Q21 message = new QBP_Q21();
@@ -67,47 +47,20 @@ PV1||I";
       terser.Set("/.QPD-3(1)-1", "@PID.5.2");
       terser.Set("/.QPD-3(1)-2", "givenName");
 
-      /// --------------------------------------------------------------- LOOPING THROUGH TESTS 
-      // Test steps as strings for now.
-      List<TestStep> testSteps = new List<TestStep> {
-        new TestStep { CaseNumber = 02, StepNumber = 10, Message = TEST_CR_02_10},
-        new TestStep { CaseNumber = 02, StepNumber = 20, Message = TEST_CR_02_20},
-        new TestStep { CaseNumber = 02, StepNumber = 30, Message = TEST_CR_02_30}
-      };
-
-      // Count the number of test cases.
-      int caseCount = 1;
-      while (true)
-      {
-        if (testSteps.Exists(s => s.CaseNumber == caseCount))
-          caseCount++;
-        else
-          break;
-      }
-
-      // Now loop through each of the test cases 
-      for (int i = 0; i < caseCount; i++)
-      {
-        // Find number of test steps for this test case
-        int stepCount = testSteps.Where(s => s.CaseNumber == i).ToList().Count;
-
-        // Loop through test steps
-        for (int j = 0; j < stepCount; j++)
-        {
-          //TODO: Modify the code for parsing and sending to generically handle all messages.
-        }
-      }
+      /// -------------------------------------------------------------------- PARSE AND SEND
+      //PipeParser Parser = new PipeParser();
+      //IMessage m = Parser.Parse(msg);
 
       // ----------------------------------------------------------------------- TESTING NEW DESIGN
       TestSuiteBuilderDirector director = new TestSuiteBuilderDirector(new TestSuiteBuilder(), @"D:\MEDIC\HL7TestingTool\HL7TestingTool\data\");
       director.BuildTestSuite();
       List<TestStep> allSteps = director.GetResult();
-      foreach (TestStep t in testSteps)
+      foreach (TestStep t in allSteps)
       {
         try
         {
-          PipeParser Parser = new PipeParser();
-          var encodedMessage = Parser.Parse($@"{t.Message}");
+          PipeParser parser = new PipeParser();
+          var encodedMessage = parser.Parse($@"{t.Message}");
           MllpMessageSender sender = new MllpMessageSender(new Uri("llp://127.0.0.1:2100"));
           Console.WriteLine("Sending and receiving MLLP message at llp:127.0.0.1:2100 ...");
 
@@ -115,7 +68,7 @@ PV1||I";
           Console.WriteLine(t.Message);
           Console.WriteLine();
           Console.WriteLine($"Response for message: TEST-CR-{t.CaseNumber}-{t.StepNumber}");
-          Console.WriteLine(Parser.Encode(sender.SendAndReceive(encodedMessage)));
+          Console.WriteLine(parser.Encode(sender.SendAndReceive(encodedMessage)));
           Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
         }
         catch (Exception e)
@@ -124,45 +77,6 @@ PV1||I";
         }
       }
       Console.ReadKey();
-
-        /// -------------------------------------------------------------------- PARSE AND SEND
-        //PipeParser Parser = new PipeParser();
-        //IMessage m = Parser.Parse(msg);
-        //NHapi.Model.V231.Message.ADT_A01 adta01 = m as NHapi.Model.V231.Message.ADT_A01;
-
-      //  // Use var here to ensure that the correct type (representing message structure) is used at runtime
-      //  var adta01_CR_02_10 = Parser.Parse(TEST_CR_02_10);
-      //Type a = adta01_CR_02_10.GetType();
-      //Console.WriteLine("type: " + a);
-      //var qpbq21_CR02_20 = Parser.Parse(TEST_CR_02_20);
-      //a = qpbq21_CR02_20.GetType();
-      //Console.WriteLine("type: " + a);
-
-      //var adta01_CR_02_30 = Parser.Parse(TEST_CR_02_30);
-      //var adta01_CR_03_10 = Parser.Parse(TEST_CR_03_10);
-
-      //// Send ADT_A01 message and process returned message
-      //MllpMessageSender sender = new MllpMessageSender(new Uri("llp://127.0.0.1:2100"));
-      //Console.WriteLine("Sending and receiving MLLP message at llp:127.0.0.1:2100 ...");
-      //while (true)
-      //{
-      //  Console.WriteLine("Response CR-02-10:");
-      //  Console.WriteLine(Parser.Encode(sender.SendAndReceive(adta01_CR_02_10)));
-
-      //  Console.WriteLine("\nResponse CR-02-20:");
-      //  Console.WriteLine(Parser.Encode(sender.SendAndReceive(qpbq21_CR02_20)));
-
-      //  //Console.WriteLine("\nResponse CR-02-30:");
-      //  //Console.WriteLine(Parser.Encode(sender.SendAndReceive(adta01_CR_01_30)));
-
-      //  Console.WriteLine("\nResponse CR-03-10:");
-      //  Console.WriteLine(Parser.Encode(sender.SendAndReceive(adta01_CR_03_10)));
-
-      //  Console.WriteLine("\nResponse CR-03-10:");
-      //  Console.WriteLine(Parser.Encode(message));
-
-      //  Console.ReadKey();  // Press a key in the console to send messages again.
-      //}
     }
   }
 }
