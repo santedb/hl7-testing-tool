b907c747da6b18b9580f5caef1f5a15d63750404
Nityan Khanna <khannani@hhsc.ca>
2022-03-02 21:13:40 -0500

    added documentation

--- HL7TestingTool/HL7TestingTool/Core/Impl/Alternate.cs
+++ HL7TestingTool/HL7TestingTool/Core/Impl/Alternate.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 
 namespace HL7TestingTool.Core.Impl
 {
+    /// <summary>
+    /// Represents an alternate value for an assertion.
+    /// </summary>
     public class Alternate
     {
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
         [XmlAttribute("value")]
         public string Value { get; set; }
     }
--- HL7TestingTool/HL7TestingTool/Core/Impl/Assertion.cs
+++ HL7TestingTool/HL7TestingTool/Core/Impl/Assertion.cs
@@ -5,17 +5,23 @@ using System.Xml.Serialization;
 namespace HL7TestingTool.Core.Impl
 {
     /// <summary>
-    /// 
+    /// Represents an assertion.
     /// </summary>
 
     public class Assertion
     {
         /// <summary>
-        /// 
+        /// Initializes a new instance of the <see cref="Assertion"/> class.
+        /// </summary>
+        public Assertion()
+        {
+            this.Alternates = new List<Alternate>();
+        }
+
+        /// <summary>
+        /// Gets or sets the list of alternate values for a given assertion.
         /// </summary>
         [XmlElement("alternate")]
-        //[XmlArray("alternates")]
-        //[XmlArrayItem("alternate", typeof(Assertion))]
         public List<Alternate> Alternates { get; set; }
 
         /// <summary>
@@ -25,27 +31,27 @@ namespace HL7TestingTool.Core.Impl
         public bool Missing { get; set; }
 
         /// <summary>
-        /// 
+        /// Gets or sets the outcome.
         /// </summary>
         [XmlIgnore]
         public bool? Outcome { get; set; }
 
         /// <summary>
-        /// 
+        /// Gets or sets the terser string.
         /// </summary>
         [XmlAttribute("terserString")]
         public string TerserString { get; set; }
 
         /// <summary>
-        /// 
+        /// Gets or sets the value to assert against.
         /// </summary>
         [XmlAttribute("value")]
         public string Value { get; set; }
 
         /// <summary>
-        /// 
+        /// Returns this instance as a string representation.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Returns this instance as a string representation.</returns>
         public override string ToString()
         {
             return this.Alternates.Any() ? $"Expected: ['{this.Value}, {string.Join(", ",this.Alternates.Select(c=> c.Value))}'] at '{this.TerserString}'"
