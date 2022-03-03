3866f4cd88e1613a905565e4fcfe503fa94cc995
Nityan Khanna <khannani@hhsc.ca>
2022-03-02 21:02:12 -0500

    added documentation

--- HL7TestingTool/HL7TestingTool/Interop/IMllpMessageSender.cs
+++ HL7TestingTool/HL7TestingTool/Interop/IMllpMessageSender.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace HL7TestingTool.Interop
+﻿namespace HL7TestingTool.Interop
 {
+    /// <summary>
+    /// Represents an MLLP message sender.
+    /// </summary>
     public interface IMllpMessageSender
-    { 
+    {
+        /// <summary>
+        /// Sends and receives a message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <returns>Returns the response message.</returns>
         string SendAndReceive(string message);
     }
 }
