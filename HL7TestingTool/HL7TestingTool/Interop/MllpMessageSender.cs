d8cfbaf4b5beaa9d2005437897b42bdb26dcd220
Shihab Khan <khanshi@hhsc.ca>
2022-03-07 12:08:51 -0500

    add support for connecting to secure endpoint via SLLP

--- HL7TestingTool/HL7TestingTool/Interop/MllpMessageSender.cs
+++ HL7TestingTool/HL7TestingTool/Interop/MllpMessageSender.cs
@@ -3,7 +3,9 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
 using System.Linq;
+using System.Net.Security;
 using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace HL7TestingTool.Interop
@@ -72,6 +74,49 @@ namespace HL7TestingTool.Interop
         }
 
         /// <summary>
+        /// Read the response from a NetworkStream and return a string representation of it
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        private string ReadResponse(SslStream stream)
+        {
+            var response = new StringBuilder();
+            var buffer = new byte[BufferSize];
+            while (!buffer.Contains((byte) 0x1c)) // Read into 1024 byte buffer until buffer contains FS character
+            {
+                var byteCount = stream.Read(buffer, 0, BufferSize);
+                var offset = 0;
+                if (buffer[offset] == '\v') // Adjust start and count of bytes read when starting with '|' and skip it
+                {
+                    offset = 1;
+                    byteCount--;
+                }
+
+                var buffString = Encoding.ASCII.GetString(buffer, offset, byteCount);
+                response.Append(buffString);
+            }
+
+            // No response when missing message header
+            //if (response.ToString().Split('|')[0] != "MSH")
+            //{
+            //    throw new Exception($"No message header returned from {this.configuration.GetValue<Uri>("Endpoint")} ... (MLLP response body could be missing)");
+            //}
+
+            return response.ToString();
+        }
+
+
+        /// <summary>
+        /// Validation for certificates
+        /// </summary>
+        private bool RemoteCertificateValidation(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            // TODO: Validate chain
+            return sslPolicyErrors == SslPolicyErrors.None;
+        }
+
+
+        /// <summary>
         /// Sends and receives a message.
         /// </summary>
         /// <param name="message">The message.</param>
@@ -98,11 +143,49 @@ namespace HL7TestingTool.Interop
                 // FS CR
                 memoryStream.Write(new byte[] { 0x1c, 0x0d }, 0, 2);
 
-                using var networkStream = client.GetStream();
-                networkStream.Write(memoryStream.ToArray(), 0, (int)memoryStream.Position);
-                networkStream.Flush();
+                switch (endpoint.Scheme)
+                {
+                    case "mllp":
+                    case "llp":
+                    {
+                        using var networkStream = client.GetStream();
+                        networkStream.Write(memoryStream.ToArray(), 0, (int)memoryStream.Position);
+                        networkStream.Flush();
+                        response = this.ReadResponse(networkStream);
+                        break;
+                    }
+                    case "sllp":
+                    {
+                        using var sslStream = new SslStream(client.GetStream(), false, 
+                            new RemoteCertificateValidationCallback(this.RemoteCertificateValidation));
+                            //sslStream.AuthenticateAsClient("SSN");
+                        var store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+                        store.Open(OpenFlags.ReadOnly);
+                        var clientCertificate = this.configuration.GetValue<string>("ClientCertificateThumbprint");
+                        if (clientCertificate == null)
+                        {
+                            this.logger.LogError("Invalid property: ClientCertificateThumbprint cannot be null when using SLLP.");
+                            throw new InvalidOperationException();
+                        }
+                        var clientCertificates = store.Certificates.Find(X509FindType.FindByThumbprint, clientCertificate, true);
+                        if (clientCertificates.Count < 1 || clientCertificates.Count > 1)
+                        {
+                            throw new ArgumentNullException();
+                        }
 
-                response = this.ReadResponse(networkStream);
+                        sslStream.AuthenticateAsClient(endpoint.Host, clientCertificates, true);
+                        sslStream.Write(memoryStream.ToArray(), 0, (int)memoryStream.Position);
+                        sslStream.Flush();
+                        response = this.ReadResponse(sslStream);
+                        break;
+                    }
+                    default:
+                        this.logger.LogError("Endpoint protocol not supported.");
+                        throw new InvalidOperationException("Protocol not supported.");
+                }
+                
+
+                
             }
             catch (Exception e)
             {
